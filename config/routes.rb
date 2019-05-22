8b4e56e78b5cab1d5adb3c6b9de5a64dd70d7e59
christophe bouquet <christophe.productowner@gmail.com>
2019-05-22 10:56:12 +0200

    premierexercecice

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cocktails do
-    resources :doses
+    resources :doses, only: [:new, :create]
   end
+  resources :doses, only: [:destroy]
+  # get '/dose/:dose_id' to 'dose#destroy'
+  root to: 'cocktails#index'
 end
